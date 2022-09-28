a1faf81a101cf22979fa91ac73fea02f0d29a907
XFFXFF <1247714429@qq.com>
2022-09-28 17:28:30 +0800

    add flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: 
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShell.x86_64-linux =
+        pkgs.mkShell {
+          buildInputs = [
+	    pkgs.nodejs
+	    pkgs.nodePackages.npm
+	  ];
+        };
+    };
+
+}
