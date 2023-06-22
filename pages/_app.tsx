2427294c53ef3b531178e6d8e265d31208011c75
xffxff <1247714429@qq.com>
2023-06-22 11:27:27 +0800

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+// this file is used to add global css to the app, see https://nextjs.org/learn/basics/assets-metadata-css/global-styles for more details
 import '../styles/global.css'
 import { AppProps } from 'next/app'
 
